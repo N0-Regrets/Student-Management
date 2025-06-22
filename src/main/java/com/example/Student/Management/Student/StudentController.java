7f58456a6f0213e981d23bd4c79f1dac3eff87fa
N0-Regrets <a.a.a.abueldahab@gmail.com>
2025-06-23 00:55:36 +0300

    add CORS configuration

--- src/main/java/com/example/Student/Management/Student/StudentController.java
+++ src/main/java/com/example/Student/Management/Student/StudentController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://student-management-a868b.web.app/")
 @RestController
 @RequestMapping("/students")
 public class StudentController {
